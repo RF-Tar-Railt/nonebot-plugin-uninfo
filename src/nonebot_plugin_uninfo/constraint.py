40ff89c36a0bde07eb343555f005233c0a13f649
RF-Tar-Railt <rf_tar_railt@qq.com>
2024-08-15 14:18:33 +0800

    :tada: init

---
+++ src/nonebot_plugin_uninfo/constraint.py
@@ -0,0 +1,116 @@
+from enum import Enum
+
+from nonebot.utils import logger_wrapper
+
+log = logger_wrapper("Plugin-Uninfo")
+
+
+class SupportAdapter(str, Enum):
+    """支持的适配器"""
+
+    # bilibili = "BilibiliLive"
+    console = "Console"
+    ding = "Ding"
+    discord = "Discord"
+    dodo = "DoDo"
+    feishu = "Feishu"
+    github = "GitHub"
+    kritor = "Kritor"
+    kook = "Kaiheila"
+    minecraft = "Minecraft"
+    mirai = "Mirai"
+    # mirai_community = "mirai2"
+    ntchat = "ntchat"
+    onebot11 = "OneBot V11"
+    onebot12 = "OneBot V12"
+    qq = "QQ"
+    red = "RedProtocol"
+    satori = "Satori"
+    telegram = "Telegram"
+    tail_chat = "Tailchat"
+
+    nonebug = "fake"
+
+
+class SupportScope(str, Enum):
+    """支持的平台范围"""
+
+    qq_client = "QQClient"
+    """QQ 协议端"""
+    qq_guild = "QQGuild"
+    """QQ 用户频道，非官方接口"""
+    qq_api = "QQAPI"
+    """QQ 官方接口"""
+    telegram = "Telegram"
+    discord = "Discord"
+    feishu = "Feishu"
+    dodo = "DoDo"
+    kook = "Kaiheila"
+    minecraft = "Minecraft"
+    github = "GitHub"
+    # bilibili = "Bilibili"
+    console = "Console"
+    ding = "Ding"
+    wechat = "WeChat"
+    """微信平台"""
+    wechat_oap = "WeChatOfficialAccountPlatform"
+    """微信公众号平台"""
+    wecom = "WeCom"
+    """企业微信平台"""
+    tail_chat = "TailChat"
+    """Tailchat平台"""
+
+    onebot12_other = "Onebot12"
+    """ob12 的其他平台"""
+    satori_other = "satori"
+    """satori 的其他平台"""
+
+    @staticmethod
+    def ensure_ob12(platform: str):
+        return {
+            "qq": SupportScope.qq_client,
+            "qqguild": SupportScope.qq_guild,
+            "discord": SupportScope.discord,
+            "wechat": SupportScope.wechat,
+            "kaiheila": SupportScope.kook,
+        }.get(platform, SupportScope.onebot12_other)
+
+    @staticmethod
+    def ensure_satori(platform: str):
+        return {
+            "chronocat": SupportScope.qq_client,
+            "onebot": SupportScope.qq_client,
+            "qq": SupportScope.qq_guild,
+            "telegram": SupportScope.telegram,
+            "discord": SupportScope.discord,
+            "feishu": SupportScope.feishu,
+            "wechat-official": SupportScope.wechat_oap,
+            "wecom": SupportScope.wecom,
+            "kook": SupportScope.kook,
+            "dingtalk": SupportScope.ding,
+        }.get(platform, SupportScope.satori_other)
+
+
+class SupportAdapterModule(str, Enum):
+    """支持的适配器的模块路径"""
+
+    # bilibili = "nonebot.adapters.bilibili"
+    console = "nonebot.adapters.console"
+    ding = "nonebot.adapters.ding"
+    discord = "nonebot.adapters.discord"
+    dodo = "nonebot.adapters.dodo"
+    feishu = "nonebot.adapters.feishu"
+    github = "nonebot.adapters.github"
+    kritor = "nonebot.adapters.kritor"
+    kook = "nonebot.adapters.kaiheila"
+    minecraft = "nonebot.adapters.minecraft"
+    mirai = "nonebot.adapters.mirai"
+    # mirai_community = "nonebot.adapters.mirai2"
+    ntchat = "nonebot.adapters.ntchat"
+    onebot11 = "nonebot.adapters.onebot.v11"
+    onebot12 = "nonebot.adapters.onebot.v12"
+    qq = "nonebot.adapters.qq"
+    red = "nonebot.adapters.red"
+    satori = "nonebot.adapters.satori"
+    telegram = "nonebot.adapters.telegram"
+    tail_chat = "nonebot_adapter_tailchat"
---
+++ src/nonebot_plugin_uninfo/model.py
@@ -0,0 +1,115 @@
+from dataclasses import dataclass, field
+from datetime import datetime, timedelta
+from enum import IntEnum
+from typing import Any, Union, Optional
+
+from nonebot.adapters import Adapter
+
+from.constraint import SupportScope, SupportAdapter
+
+
+class ChannelType(IntEnum):
+    TEXT = 0
+    DIRECT = 1
+    CATEGORY = 2
+    VOICE = 3
+
+
+@dataclass
+class Guild:
+    id: str
+    name: Optional[str] = None
+    avatar: Optional[str] = None
+
+
+@dataclass
+class Channel:
+    id: str
+    type: ChannelType
+    name: Optional[str] = None
+    parent: Optional[Guild] = None
+
+
+@dataclass
+class User:
+    id: str
+    name: Optional[str] = None
+    """用户名"""
+    nick: Optional[str] = None
+    """用户昵称"""
+    avatar: Optional[str] = None
+    gender: str = "unknown"
+
+
+@dataclass
+class Role:
+    id: str
+    level: int = 0
+    name: Optional[str] = None
+
+
+@dataclass
+class MuteInfo:
+    muted: bool
+    """是否被禁言"""
+    duration: timedelta
+    """禁言时长"""
+    start_at: Optional[datetime] = None
+    """禁言开始时间"""
+
+
+@dataclass
+class Member:
+    id: str
+    nick: Optional[str] = None
+    """群员昵称"""
+    avatar: Optional[str] = None
+    role: Optional[Role] = None
+    mute: Optional[MuteInfo] = None
+    joined_at: Optional[datetime] = None
+
+
+@dataclass
+class Session:
+    self_id: str
+    """机器人id"""
+    adapter: Union[str, type[Adapter], SupportAdapter]
+    """适配器名称，若为 None 则需要明确指定 Bot 对象"""
+
+    guild: Guild
+    channel: Channel
+    user: User
+    member: Optional[Member] = None
+    scope: Union[str, SupportScope, None] = None
+    """适配器范围，相比 adapter 更指向实际平台"""
+    platform: Union[str, set[str], None] = None
+    """平台名称，仅当目标适配器存在多个平台时使用"""
+
+    @property
+    def is_private(self) -> bool:
+        return self.channel.type == ChannelType.DIRECT
+
+    @property
+    def is_channel(self) -> bool:
+        return self.channel.id == self.guild.id
+
+# @dataclass
+# class Scene:
+#     id: str
+#     """目标id；若为群聊则为group_id或者channel_id，若为私聊则为user_id"""
+#     parent_id: str
+#     """父级id；若为频道则为guild_id，其他情况下可能为空字符串（例如 Feishu 下可作为部门 id）"""
+#     channel: bool
+#     """是否为频道场景，仅当目标平台符合频道概念时"""
+#     private: bool
+#     """是否为私聊场景"""
+#     self_id: Union[str, None]
+#     """机器人id，若为 None 则 Bot 对象会随机选择"""
+#     scope: Union[str, SupportScope, None] = None
+#     """适配器范围，用于传入内置的特定选择器"""
+#     adapter: Union[str, type[Adapter], SupportAdapter, None] = None
+#     """适配器名称，若为 None 则需要明确指定 Bot 对象"""
+#     platform: Union[str, set[str], None] = None
+#     """平台名称，仅当目标适配器存在多个平台时使用"""
+#     extra: dict[str, Any] = field(default_factory=dict)
+#     """额外信息，用于适配器扩展"""
