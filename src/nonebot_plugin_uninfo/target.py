88b43234b4d222e3bb211cbaa0c54cff8bf75ce8
RF-Tar-Railt <rf_tar_railt@qq.com>
2024-09-22 00:23:15 +0800

    :sparkles: to_target without self info

--- src/nonebot_plugin_uninfo/target.py
+++ src/nonebot_plugin_uninfo/target.py
@@ -1,5 +1,6 @@
-from typing import TYPE_CHECKING, Union, overload
+from typing import TYPE_CHECKING, Literal, Union, overload
 
+from .constraint import SupportScope
 from .model import BasicInfo, Member, Scene, Session, User
 
 if TYPE_CHECKING:
@@ -7,14 +8,24 @@ if TYPE_CHECKING:
 
 
 @overload
-def to_target(model: Session) -> "Target": ...
+def to_target(model: Session, *, without_self: bool = False) -> "Target": ...
 
 
 @overload
 def to_target(model: Union[User, Member, Scene], info: BasicInfo) -> "Target": ...
 
 
-def to_target(model: Union[Session, User, Member, Scene], info: Union[BasicInfo, None] = None) -> "Target":
+@overload
+def to_target(
+    model: Union[User, Member, Scene], info: Union[str, SupportScope], *, without_self: Literal[True]
+) -> "Target": ...
+
+
+def to_target(
+    model: Union[Session, User, Member, Scene],
+    info: Union[BasicInfo, str, SupportScope, None] = None,
+    without_self: bool = False,
+) -> "Target":
     try:
         from nonebot_plugin_alconna import SupportAdapter as AlconnaSupportAdapter
         from nonebot_plugin_alconna import SupportScope as AlconnaSupportScope
@@ -24,6 +35,14 @@ def to_target(model: Union[Session, User, Member, Scene], info: Union[BasicInfo,
     if isinstance(model, Session):
         scene_id = model.scene.id
         scene_parent_id = model.scene.parent.id if model.scene.parent else ""
+        if without_self:
+            return Target(
+                scene_id,
+                scene_parent_id,
+                model.scene.is_private,
+                model.scene.is_channel,
+                scope=AlconnaSupportScope(model.scope),
+            )
         return Target(
             scene_id,
             scene_parent_id,
@@ -35,21 +54,27 @@ def to_target(model: Union[Session, User, Member, Scene], info: Union[BasicInfo,
         )
     if not info:
         raise ValueError("info is required when model is not Session")
+    if not without_self and not isinstance(info, dict):
+        raise ValueError("info must be BasicInfo when model is not Session and without_self is False")
+    if isinstance(info, str):
+        basic = {"scope": AlconnaSupportScope(info)}
+    else:
+        basic = {
+            "self_id": info["self_id"],
+            "adapter": AlconnaSupportAdapter(info["adapter"]),
+            "scope": AlconnaSupportScope(info["scope"]),
+        }
     if isinstance(model, User):
         return Target(
             model.id,
             private=True,
-            self_id=info["self_id"],
-            adapter=AlconnaSupportAdapter(info["adapter"]),
-            scope=AlconnaSupportScope(info["scope"]),
+            **basic,  # type: ignore
         )
     elif isinstance(model, Member):
         return Target(
             model.user.id,
             private=True,
-            self_id=info["self_id"],
-            adapter=AlconnaSupportAdapter(info["adapter"]),
-            scope=AlconnaSupportScope(info["scope"]),
+            **basic,  # type: ignore
         )
     elif isinstance(model, Scene):
         scene_id = model.id
@@ -59,9 +84,7 @@ def to_target(model: Union[Session, User, Member, Scene], info: Union[BasicInfo,
             scene_parent_id,
             model.is_private,
             model.is_channel,
-            self_id=info["self_id"],
-            adapter=AlconnaSupportAdapter(info["adapter"]),
-            scope=AlconnaSupportScope(info["scope"]),
+            **basic,  # type: ignore
         )
     else:
         raise ValueError("model must be Session, User, Member or Scene")
